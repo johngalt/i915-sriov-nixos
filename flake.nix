8353ee20520169768137db1863dd26a9214229ea
Corbin Wunderlich <corbin.wunderlich@gmail.com>
2025-03-11 11:15:02 -0400

    Update flake.lock

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS module which provides a kernel and dkms module for i915 SR-IOV";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/a0f3e10d94359665dba45b71b4227b0aeb851f8e";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
   outputs = {nixpkgs, ...}: let
