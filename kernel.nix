0234f657b9c5637d0744c60c5ca176de8d51126a
johngalt <322757+johngalt@users.noreply.github.com>
2025-05-21 15:09:29 -0500

    pin kernel to 6.12.28

--- kernel.nix
+++ kernel.nix
@@ -1,10 +1,10 @@
 {
-  linux_6_12,
+  linux_6_12_28,
   lib,
   ...
 }:
 
-linux_6_12.override {
+linux_6_12_28.override {
   structuredExtraConfig = with lib.kernel; {
     DRM_I915_PXP = yes;
     INTEL_MEI_PXP = module;
