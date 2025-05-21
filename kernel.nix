46dbbd1cf4d4b7452a1aa182c6b9f92bce8b1b36
johngalt <322757+johngalt@users.noreply.github.com>
2025-05-21 15:34:28 -0500

    that didnt work, reverting back to 6.12

--- kernel.nix
+++ kernel.nix
@@ -1,10 +1,10 @@
 {
-  linux_6_12_28,
+  linux_6_12,
   lib,
   ...
 }:
 
-linux_6_12_28.override {
+linux_6_12.override {
   structuredExtraConfig = with lib.kernel; {
     DRM_I915_PXP = yes;
     INTEL_MEI_PXP = module;
